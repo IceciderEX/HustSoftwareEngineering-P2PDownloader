fd575c1ae15d6d58ab036c3fc5ad43dca6899be2
li-xin_yu <2605523489@qq.com>
2023-11-06 17:00:17 +0800

    no message

---
+++ src/m3u8/test_m3u8.py
@@ -0,0 +1,3 @@
+import m3u8
+if __name__ == "__main__":
+    m3u8.jiekou('https://videotx-platform.cdn.huya.com/leaf/1048585/1542649971/61151795/14530912_81706ddc761d4ca7279b821026cb4908_264_720_6_ai.m3u8','D:\\University\\early_3rd\\m3u8download','lxy.mp4')
