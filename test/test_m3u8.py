d6672a835eb6cef9b6441e5aa7a1fca16ae2d78e
Icecider <1109413426@qq.com>
2023-11-09 13:22:19 +0000

    update test/test_m3u8.py.
    Signed-off-by: Icecider. <1109413426@qq.com>

--- test/test_m3u8.py
+++ test/test_m3u8.py
@@ -1,3 +1,5 @@
-import m3u8
-if __name__=="__main__":
-    m3u8.jiekou('https://videotx-platform.cdn.huya.com/leaf/1048585/1542649971/61151795/14530912_81706ddc761d4ca7279b821026cb4908_264_720_6_ai.m3u8','G:/','66.mp4')
+from src.m3u8.m3u8 import interface_ui
+
+if __name__ == "__main__":
+    interface_ui('https://videotx-platform.cdn.huya.com/leaf/1048585/1542649971/61151795'
+                      '/14530912_81706ddc761d4ca7279b821026cb4908_264_720_6_ai.m3u8','D:/','123.mp4')
